2bc80b099ea26ccf10429f5942161290f74581ab
WangWenQian12 <1360368865@qq.com>
2019-06-16 19:48:00 +0800

    RandLetter

---
+++ RandLetter/src/randletter11/RandLetter11.java
@@ -0,0 +1,8 @@
+package randletter11;
+
+public class RandLetter11 {
+
+    public static void main(String[] args) {
+        System.out.println((char)(65+(int)(Math.random()*26)));        
+    }  
+}

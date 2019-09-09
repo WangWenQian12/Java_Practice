167592e90cda5e0941e86e0f82ce3a2b80fa1ac3
WangWenQian12 <1360368865@qq.com>
2019-09-09 22:13:22 +0800

    求A+B

---
+++ JavaSE/蓝桥杯/add/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println ("Please inter a:");
+        int a = input.nextInt();
+        System.out.println ("Please inter b:");
+        int b = input.nextInt();
+        System.out.println (a+b);
+
+    }
+}

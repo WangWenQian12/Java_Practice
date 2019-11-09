b0688f69549e4f8831f763ccf0a3a0203fcfe42a
WangWenQian12 <1360368865@qq.com>
2019-11-09 17:20:40 +0800

    检测输入字符串括号是否匹配

---
+++ JavaSE/每日一题/Parenthesis1/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class Main {
+
+    //检测输入字符串括号是否匹配
+    //思路：
+    //1.遍历字符串，左括号入栈，当遇到右括号时，栈顶元素出栈，看是否与当前右括号匹配
+
+    public static boolean chkParenthesis(String str,int n){
+        Stack<Character> stack = new Stack<> ();
+        for(int i = 0;i < str.length ();i++){
+            char c = str.charAt (i);
+            switch(c){
+                case '(':
+                    stack.add (c);
+                    break;
+                case ')':
+                    if(stack.isEmpty ()){
+                        return false;
+                    }
+                    if(stack.remove (stack.size ()-1) == '('){
+                        return true;
+                    }
+                    break;
+                    default:
+                        break;
+
+            }
+        }
+        if(stack.isEmpty ()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println (chkParenthesis ("(()())",6));
+    }
+}

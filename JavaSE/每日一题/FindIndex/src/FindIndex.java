c8072b9ed6f0f3dceb2d241e7a4be4d9b43b47d9
WangWenQian12 <1360368865@qq.com>
2020-03-05 20:29:30 +0800

    找到数组中下标和为k的俩个元素

---
+++ JavaSE/每日一题/FindIndex/src/FindIndex.java
@@ -0,0 +1,41 @@
+﻿import java.util.Scanner;
+
+public class FindIndex {
+
+    //找到数组中下标和为k的俩个元素
+    public static void getIndex(int arr[],int len,int k){
+        int left = 0;
+        int right = len-1;
+        while(arr[left]<arr[right]){
+            if(arr[left]+arr[right]<k){
+                left++;
+            }
+            else if(arr[left]+arr[right]>k){
+                right--;
+            }
+            else{
+                System.out.println("和为"+k+"的下标为"+left+"和"+right);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+
+        System.out.println("Please inter the number of array:");
+        int n = input.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Please inter the array:");
+        for(int i =0;i<n;i++){
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Please inter the sum:");
+        int k = input.nextInt();
+
+        getIndex(arr,n,k);
+
+    }
+}

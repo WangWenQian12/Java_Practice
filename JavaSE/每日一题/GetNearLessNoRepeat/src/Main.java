8c2623c5452363d311eea779a3823581b8c24e80
WangWenQian12 <1360368865@qq.com>
2020-06-03 20:44:12 +0800

    给定一个不含有重复值的数组 arr，找到每一个 i 位置左边和右边离 i 位置最近且值比 arr[i] 小的位置（利用栈）

--- JavaSE/每日一题/GetNearLessNoRepeat/src/Main.java
+++ JavaSE/每日一题/GetNearLessNoRepeat/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Stack;
 public class Main {
 
     // 给定一个不含有重复值的数组 arr，找到每一个 i 位置左边和右边离 i 位置最近且值比 arr[i] 小的位置。
+    // 利用栈，如果新插入的数字小于栈顶元素时，记录（全程用数组下标记录）
 
     public static int[][] getNearLessNoRepeat(int[] arr){
         int[][] res = new int[arr.length][2];

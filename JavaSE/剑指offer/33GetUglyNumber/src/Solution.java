e2bdc57c465b9b62c3b2dcc542da7f950270def3
WangWenQian12 <1360368865@qq.com>
2020-08-28 21:46:36 +0800

    丑数  把只包含质因子2、3和5的数称作丑数

---
+++ JavaSE/剑指offer/33GetUglyNumber/src/Solution.java
@@ -0,0 +1,42 @@
+public class Solution {
+
+    // 丑数
+    // 把只包含质因子2、3和5的数称作丑数
+
+    public static int GetUglyNumber_Solution(int index) {
+        if(index <= 0){
+            return 0;
+        }
+
+        // 三个指针定位
+        int p2 = 0;
+        int p3 = 0;
+        int p5 = 0;
+        int[] result = new int[index];
+        result[0] = 1;
+
+        for(int i = 1;i < index;i++){
+            //之后大概率会有重复，率先将最小的值放入
+            result[i] = Math.min (result[p2] * 2, Math.min (result[p3] * 3, result[p5] * 5));
+            //当前结果数组是2，3，5其中某一位的倍数，则该指针代表向后移动
+            if(result[i] == result[p2]*2){
+                p2++;
+            }
+            if(result[i] == result[p3]*3){
+                p3++;
+            }
+            if(result[i] == result[p5]*5){
+                p5++;
+            }
+
+        }
+        return result[index-1];
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println (GetUglyNumber_Solution (10));
+    }
+
+}
